d42dc066dc593e1a8dd24f20c6b8f778efb239d6
AlekseyMinai <aleksejzuke@gmail.com>
2022-04-25 00:30:32 +0300

    Добавил гитигнор и файлы ресурсов

--- app/src/main/java/com/alexey/minay/mviviewmodel/presentation/NotesStore.kt
+++ app/src/main/java/com/alexey/minay/mviviewmodel/presentation/NotesStore.kt
@@ -1,9 +1,28 @@
 package com.alexey.minay.mviviewmodel.presentation
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.alexey.minay.lib.ViewModelStore
 
 class NotesStore(
     reducer: NotesReducer,
-    executor: NotesActor,
+    actor: NotesActor,
     initialState: NotesState = NotesState.default()
-) : ViewModelStore<NotesState, NotesAction, NotesEffect, NotesResult>(executor, reducer, initialState)
+) : ViewModelStore<NotesState, NotesAction, NotesEffect, NotesResult>(
+    actor,
+    reducer,
+    initialState
+) {
+
+    class Factory(
+        private val reducer: NotesReducer,
+        private val actor: NotesActor,
+    ) : ViewModelProvider.Factory {
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return NotesStore(reducer, actor) as T
+        }
+
+    }
+
+}
